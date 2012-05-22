8191a1a04b7a2249e72134d262441ed4e3437840
Mario Estrada <mario.estrada@gmail.com>
2012-05-22 12:29:10 -0500

    added examples js

---
+++ examples.js
@@ -0,0 +1,105 @@
+$(function()
+{
+   $('#map').gMap();
+   
+   $('#map_controls').gMap(
+   {
+        latitude: -2.206,
+        longitude: -79.897,
+        maptype: 'TERRAIN', // 'HYBRID', 'SATELLITE', 'ROADMAP' or 'TERRAIN'
+        zoom: 8,
+        controls: {
+            panControl: true,
+            zoomControl: false,
+            mapTypeControl: true,
+            scaleControl: false,
+            streetViewControl: false,
+            overviewMapControl: false
+        }
+   });
+   
+   $('#map_addresses').gMap({
+        address: "Quito, Ecuador",
+        zoom: 5,
+		markers:[
+			{
+				latitude: -2.2014,
+				longitude: -80.9763,
+				html: "_latlng"
+			},
+			{
+				address: "Guayaquil, Ecuador",
+				html: "My Hometown",
+				popup: true
+			},
+			{
+				address: "Galapagos, Ecuador",
+				html: "_address"
+			}
+		]
+    });
+
+	$("#map_extended").gMap({
+		controls: false,
+		scrollwheel: true,
+		maptype: 'TERRAIN',
+		markers: [
+			{
+				latitude: 47.670553,
+				longitude: 9.588479,
+				icon: {
+					image: "images/gmap_pin_orange.png",
+					iconsize: [26, 46],
+					iconanchor: [12,46]
+				}
+			},
+	        {
+				latitude: 47.65197522925437,
+				longitude: 9.47845458984375
+			},
+			{
+				latitude: 47.594996,
+				longitude: 9.600708,
+				icon: {
+					image: "images/gmap_pin_grey.png",
+					iconsize: [26, 46],
+					iconanchor: [12,46]
+				}
+			}
+		],
+		icon: {
+			image: "images/gmap_pin.png", 
+			iconsize: [26, 46],
+			iconanchor: [12, 46]
+		},
+		latitude: 47.58969,
+		longitude: 9.473413,
+		zoom: 10
+	});
+	
+	// Detect user location
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position)
+		{
+			$('#map_controls').gMap('addMarker', {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+				content: 'You are here!',
+				icon: {
+					image: "images/gmap_pin.png", 
+					iconsize: [26, 46],
+					iconanchor: [12, 46]
+				},
+				popup: true
+			});
+			$('#map_controls').gMap('centerAt', {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+				zoom: 8
+			});
+		}, function()
+		{
+			//console.log('Couldn\'t find you :(');
+		});
+	}
+});
